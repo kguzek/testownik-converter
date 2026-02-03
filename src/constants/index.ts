50819d9b0183aed1aaa882f8ddac823c9297a286
Konrad Guzek <konrad@guzek.uk>
2026-02-03 20:08:52 +0100

    fix(cli): read version from package.json

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,9 +1,10 @@
 export const AUTHOR_EMAIL = "konrad@guzek.uk";
 export const AUTHOR_NAME = "Konrad Guzek";
 export const AUTHOR_URL = "https://github.com/kguzek";
+import pkg from "../../package.json";
 
 export const REPO_NAME = "Testownik Converter";
 export const REPO_SLUG = "testownik-converter";
 export const REPO_URL = `${AUTHOR_URL}/${REPO_SLUG}`;
 
-export const VERSION = process.env.npm_package_version || "1.0.3";
+export const VERSION = pkg.version;
